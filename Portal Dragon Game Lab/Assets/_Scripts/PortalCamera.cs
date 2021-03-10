737973092f8478952b157c52211a39fa20d160b7
Winningtopas <jay07@live.nl>
2021-03-10 11:58:11 +0100

    Started on making the code ready for recursion.

--- Portal Dragon Game Lab/Assets/_Scripts/PortalCamera.cs
+++ Portal Dragon Game Lab/Assets/_Scripts/PortalCamera.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class PortalCamera : MonoBehaviour {
 
@@ -11,19 +12,51 @@ public class PortalCamera : MonoBehaviour {
     [SerializeField]
     private Vector3 relativePos;
 
+    [SerializeField]
+    private int iterations = 7;
+
+
     // Update is called once per frame
     void Update () {
 
-        relativePos = portal.InverseTransformPoint(playerCamera.transform.position);
-        relativePos = Quaternion.Euler(0.0f, 180.0f, 0.0f) * relativePos;
-        transform.position = otherPortal.transform.TransformPoint(relativePos);
+    }
 
-        Quaternion relativeRot = Quaternion.Inverse(portal.rotation) * playerCamera.transform.rotation;
-        relativeRot = Quaternion.Euler(0.0f, 180.0f, 0.0f) * relativeRot;
-        transform.rotation = otherPortal.transform.rotation * relativeRot;
+    void OnEnable()
+    {
+        RenderPipelineManager.endCameraRendering += RenderPipelineManager_endCameraRendering;
+    }
+    void OnDisable()
+    {
+        RenderPipelineManager.endCameraRendering -= RenderPipelineManager_endCameraRendering;
+    }
+    private void RenderPipelineManager_endCameraRendering(ScriptableRenderContext context, Camera camera)
+    {
+        OnPreRender();
+    }
 
-        //adjusting the near clipping plane
-        GetComponent<Camera>().nearClipPlane = Vector3.Distance(transform.position, otherPortal.position);
+    private void OnPreRender()
+    {
+        for (int i = iterations - 1; i >= 0; --i)
+        {
+            RenderCamera(i);
+        }
+    }
+
+    void RenderCamera(int iterationID)
+    {
+        for (int i = 0; i <= iterationID; ++i)
+        {
+            relativePos = portal.InverseTransformPoint(playerCamera.transform.position);
+            relativePos = Quaternion.Euler(0.0f, 180.0f, 0.0f) * relativePos;
+            transform.position = otherPortal.transform.TransformPoint(relativePos);
+
+            Quaternion relativeRot = Quaternion.Inverse(portal.rotation) * playerCamera.transform.rotation;
+            relativeRot = Quaternion.Euler(0.0f, 180.0f, 0.0f) * relativeRot;
+            transform.rotation = otherPortal.transform.rotation * relativeRot;
+
+            //adjusting the near clipping plane
+            GetComponent<Camera>().nearClipPlane = Vector3.Distance(transform.position, otherPortal.position);
+        }
     }
 
     public void AssignPortals(GameObject portalGameObject, GameObject otherPortalGameObject)
